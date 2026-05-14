e5cd55d2e92d144ff6b5532e995d932500e7c3cc
Chilohc97 <73367604+Chilohc97@users.noreply.github.com>
2026-05-13 21:04:57 -0400

    Implemented selection and insertion sort, and added unit tests

--- Arrays/Py/algos.py
+++ Arrays/Py/algos.py
@@ -16,3 +16,39 @@ def bubble_sort(arr):
     
     return arr
 
+# time complexity of O(n^2)
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min = arr[i]
+        index = i
+        for j in range(i, len(arr)):
+            if arr[j] < min:
+                min = arr[j]
+                index = j
+        swap_num = arr[index]
+        arr[index] = arr[i]
+        arr[i] = swap_num
+    
+    return arr
+
+
+# time complexity of O(n^2)
+def insertion_sort(arr):
+    for i in range(len(arr)-1):
+        index = i+1
+        for j in range(i+1):
+            if arr[i+1] > arr[i]:
+                break
+            if arr[j] > arr[i+1]:
+                index = j
+                break
+        
+        if index != i+1:
+            swap_num = arr[i+1]
+            for a in range(i, index-1, -1):
+                arr[a+1] = arr[a]
+            arr[index] = swap_num
+    
+    return arr
+
+
--- Arrays/Py/test_algos.py
+++ Arrays/Py/test_algos.py
@@ -28,5 +28,60 @@ class TestBubbleSort(unittest.TestCase):
         self.assertEqual(output, algos.bubble_sort(input))
 
 
+class TestSelectionSort(unittest.TestCase):
+     def test_unordered_array(self):
+        input = [5,17,59,1,53,102,3,53,21,116,53,22]
+        output = [1, 3, 5, 17, 21, 22, 53, 53, 53, 59, 102, 116]
+        self.assertEqual(output, algos.selection_sort(input))
+    
+     def test_single_element(self):
+        input = [65]
+        output = [65]
+        self.assertEqual(output, algos.selection_sort(input))
+
+     def test_empty_array(self):
+        input = []
+        output = []
+        self.assertEqual(output, algos.selection_sort(input))
+
+     def test_already_sorted(self):
+        input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertEqual(output, algos.selection_sort(input))
+
+     def test_negative_values(self):
+        input = [-1, -9, -2, -7, -5, -3, -12, -24, -3]
+        output = [-24, -12, -9, -7, -5, -3, -3, -2, -1]
+        self.assertEqual(output, algos.selection_sort(input))
+
+
+class TestInsertionSort(unittest.TestCase):
+    def test_unordered_array(self):
+        input = [5,17,59,1,53,102,3,53,21,116,53,22]
+        output = [1, 3, 5, 17, 21, 22, 53, 53, 53, 59, 102, 116]
+        self.assertEqual(output, algos.insertion_sort(input))
+    
+    def test_single_element(self):
+        input = [65]
+        output = [65]
+        self.assertEqual(output, algos.insertion_sort(input))
+
+    def test_empty_array(self):
+        input = []
+        output = []
+        self.assertEqual(output, algos.insertion_sort(input))
+
+    def test_already_sorted(self):
+        input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertEqual(output, algos.insertion_sort(input))
+
+    def test_negative_values(self):
+        input = [-1, -9, -2, -7, -5, -3, -12, -24, -3]
+        output = [-24, -12, -9, -7, -5, -3, -3, -2, -1]
+        self.assertEqual(output, algos.insertion_sort(input))
+
+
+
 if __name__ == '__main__':
     unittest.main()
