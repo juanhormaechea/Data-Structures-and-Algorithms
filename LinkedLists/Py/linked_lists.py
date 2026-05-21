d19eff81776e22b1fdce6a1b021d9647df73d3ff
Chilohc97 <73367604+Chilohc97@users.noreply.github.com>
2026-05-21 17:43:32 +0200

    implemented remove, search, and sort list methods. Added unit tests

--- LinkedLists/Py/linked_lists.py
+++ LinkedLists/Py/linked_lists.py
@@ -75,8 +75,83 @@ class LinkedList:
         new_node.next = next_node
         self._size+=1
         return
-        
+    
+    def remove_node(self, index: int) -> int | None:
+        if index < 0 or index > self._size-1:
+            raise ValueError("index out of bounds")
 
+        if index == 0:
+            assert self.head is not None
+            value = self.head.data
+            self.head = self.head.next
+            self._size-=1
+            return value
+        
+        prev = self.head
+        for i in range(1, index):
+            assert prev is not None
+            prev = prev.next
+        
+        assert prev is not None
+        node_to_remove = prev.next
+        assert node_to_remove is not None
+        node_to_link = node_to_remove.next
+        value = node_to_remove.data
+        prev.next = node_to_link
+        self._size-=1
+        return value
+    
+    def search_node(self, node: int) -> int | None:
+        head = self.head
+        index = 0
+        while head is not None:
+            if head.data == node:
+                return index
+            head = head.next
+            index+=1
+        
+        return None
+    
+    
+    def __merge(self, list1: Node | None, list2: Node | None) -> Node | None:
+        if list1 is None:
+            return list2
+        
+        if list2 is None:
+            return list1
+        
+        if list1.data <= list2.data:
+            list1.next = self.__merge(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.__merge(list1, list2.next)
+            return list2
+    
+    def sort_list(self, list=None, size=None) -> Node | None:
+        if self._size == 0:
+            return None
+        
+        if size is None:
+            return self.sort_list(self.head, self._size)
+
+        if size <= 1:
+            return list
+
+        mid = size//2
+        prev = list
+        for i in range(0, mid-1):
+            prev = prev.next
+        
+        head_right = prev.next
+        prev.next = None
+
+        sorted_left = self.sort_list(list, mid)
+        sorted_right = self.sort_list(head_right, size-mid)
+
+        self.head = self.__merge(sorted_left, sorted_right)
+        return self.head
+
+            
      
     
     
--- LinkedLists/Py/test_linked_lists.py
+++ LinkedLists/Py/test_linked_lists.py
@@ -119,6 +119,167 @@ class TestInsert(unittest.TestCase):
         for i in range(1, 4):
             head = head.next
         self.assertEqual(24, head.data)
+
+
+class TestRemove(unittest.TestCase):
+    def test_index_out_of_bounds(self):
+        list = linked_lists.LinkedList()
+        self.assertRaises(ValueError, list.remove_node, 0)
+        for i in range(0, 5):
+            list.append(i)
+        self.assertRaises(ValueError, list.remove_node, -1)
+        self.assertRaises(ValueError, list.remove_node, 5)
+    
+    def test_first_node(self):
+        list = linked_lists.LinkedList()
+        list.append(1)
+        value = list.remove_node(0)
+        self.assertEqual(1, value)
+        self.assertEqual(0, list.get_size())
+        self.assertIsNone(list.head)
+        for i in range(1, 5):
+            list.append(i)
+        value = list.remove_node(0)
+        self.assertEqual(1, value)
+        self.assertEqual(2, list.head.data)
+        self.assertEqual(3, list.get_size())    
+    
+    def test_last_node(self):
+        list = linked_lists.LinkedList()
+        for i in range(0, 5):
+            list.append(i)
+        value = list.remove_node(4)
+        self.assertEqual(4, value)
+        self.assertEqual(4, list.get_size())
+        head = list.head
+        while head.next is not None:
+            head = head.next
+        self.assertEqual(3, head.data)
+
+    def test_middle_node(self):
+        list = linked_lists.LinkedList()
+        for i in range(0, 7):
+            list.append(i)
+        value = list.remove_node(4)
+        self.assertEqual(4, value)
+        self.assertEqual(6, list.get_size())
+        prev = list.head
+        for i in range(1, 4):
+            prev = prev.next
+        next = prev.next
+        self.assertEqual(3, prev.data)
+        self.assertEqual(5, next.data)
+    
+
+class TestSearch(unittest.TestCase):
+    def test_search_empty_list(self):
+        list = linked_lists.LinkedList()
+        index = list.search_node(1)
+        self.assertIsNone(index)
+    
+    def test_search_node_not_present(self):
+        list = linked_lists.LinkedList()
+        for i in range(0, 10):
+            list.append(i)
+        index = list.search_node(10)
+        self.assertIsNone(index)
+    
+    def test_search_first_node(self):
+        list = linked_lists.LinkedList()
+        list.append(0)
+        index = list.search_node(0)
+        self.assertEqual(0, index)
+        for i in range(1, 10):
+            list.append(i)
+        index = list.search_node(0)
+        self.assertEqual(0, index)
+    
+    def test_search_last_node(self):
+        list = linked_lists.LinkedList()
+        for i in range(0, 10):
+            list.append(i)
+        index = list.search_node(9)
+        self.assertEqual(9, index)
+        
+
+class TestSort(unittest.TestCase):
+    def test_empty_list(self):
+        list = linked_lists.LinkedList()
+        node = list.sort_list()
+        self.assertIsNone(node)
+        self.assertEqual(node, list.head)
+    
+    def test_one_node(self):
+        list = linked_lists.LinkedList()
+        list.append(1)
+        value = list.sort_list()
+        self.assertEqual(1, list.get_size())
+        self.assertEqual(1, list.head.data)
+        self.assertEqual(value, list.head)
+    
+    def test_already_sorted(self):
+        list = linked_lists.LinkedList()
+        for i in range(0, 10):
+            list.append(i)
+        list.sort_list()
+        self.assertEqual(10, list.get_size())
+        head = list.head
+        for i in range(0, 10):
+            self.assertEqual(i, head.data)
+            head = head.next
+    
+    def test_even_size(self):
+        input_arr = [5, 2, 8, 1, 9, 3, 7, 4, 6, 0]
+        expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        list = linked_lists.LinkedList()
+        for i in input_arr:
+            list.append(i)
+        list.sort_list()
+        self.assertEqual(10, list.get_size())
+        head = list.head
+        for i in range(0, 10):
+            self.assertEqual(head.data, expected[i])
+            head = head.next
+    
+    def test_odd_size(self):
+        input_arr = [5, 2, 8, 1, 9, 3, 7, 4, 6]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        list = linked_lists.LinkedList()
+        for i in input_arr:
+            list.append(i)
+        list.sort_list()
+        self.assertEqual(9, list.get_size())
+        head = list.head
+        for i in range(0, 9):
+            self.assertEqual(head.data, expected[i])
+            head = head.next
+    
+    def test_reverse_sorted(self):
+        input_arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+        expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        list = linked_lists.LinkedList()
+        for i in input_arr:
+            list.append(i)
+        list.sort_list()
+        self.assertEqual(10, list.get_size())
+        head = list.head
+        for i in range(0, 10):
+            self.assertEqual(head.data, expected[i])
+            head = head.next
+    
+    def test_negatives(self):
+        input_arr = [-5, -3, -8, -2, -7, -4, -9, -6, -1, -0]
+        expected = [-9, -8, -7, -6, -5, -4, -3, -2, -1, -0]
+        list = linked_lists.LinkedList()
+        for i in input_arr:
+            list.append(i)
+        list.sort_list()
+        self.assertEqual(10, list.get_size())
+        head = list.head
+        for i in range(0, 10):
+            self.assertEqual(head.data, expected[i])
+            head = head.next
+        
         
             
         
