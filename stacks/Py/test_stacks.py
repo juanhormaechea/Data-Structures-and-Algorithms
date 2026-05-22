6e994cfe3deefa221d41bfd5666c14fa17180135
Chilohc97 <73367604+Chilohc97@users.noreply.github.com>
2026-05-22 16:15:54 +0200

    Added peek, isEmpty and stackSize methods. Added unit tests

--- stacks/Py/test_stacks.py
+++ stacks/Py/test_stacks.py
@@ -41,6 +41,59 @@ class TestPop(unittest.TestCase):
         self.assertEqual(9, stack._size)
         self.assertEqual(9, value)
         self.assertEqual(8, stack.head.data)
+    
+
+class TestPeek(unittest.TestCase):
+    def test_empty_list(self):
+        stack = stacks.Stack()
+        self.assertIsNone(stack.peek())
+    
+    def test_one_node(self):
+        stack = stacks.Stack()
+        stack.push(1)
+        value = stack.peek()
+        self.assertEqual(1, value)
+    
+    def test_full_list(self):
+        stack = stacks.Stack()
+        for i in range(0, 10):
+            stack.push(i)
+        
+        value = stack.peek()
+        self.assertEqual(9, value)
+
+class TestIsEmpty(unittest.TestCase):
+    def test_empty_list(self):
+        stack = stacks.Stack()
+        self.assertTrue(stack.isEmpty())
+    
+    def test_one_node(self):
+        stack = stacks.Stack()
+        stack.push(1)
+        self.assertFalse(stack.isEmpty())
+    
+    def test_full_list(self):
+        stack = stacks.Stack()
+        for i in range(0, 10):
+            stack.push(i)
+        self.assertFalse(stack.isEmpty())
+
+class TestStackSize(unittest.TestCase):
+    def test_empty_list(self):
+        stack = stacks.Stack()
+        self.assertEqual(0, stack.stackSize())
+    
+    def test_one_node(self):
+        stack = stacks.Stack()
+        stack.push(1)
+        self.assertEqual(1, stack.stackSize())
+    
+    def test_full_list(self):
+        stack = stacks.Stack()
+        for i in range(0, 10):
+            stack.push(i)
+        self.assertEqual(10, stack.stackSize())
+    
 
 
 if __name__ == '__main__':
