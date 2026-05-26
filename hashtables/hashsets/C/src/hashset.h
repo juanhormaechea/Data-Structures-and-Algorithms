a2e87ef96468486177f12303366542c9a9a4128b
Chilohc97 <73367604+Chilohc97@users.noreply.github.com>
2026-05-26 12:23:42 +0200

    Wrote unittests for hashset in C

--- hashtables/hashsets/C/src/hashset.h
+++ hashtables/hashsets/C/src/hashset.h
@@ -6,31 +6,29 @@ typedef struct Node Node;
 typedef struct HashSet HashSet;
 
 struct Node {
-    int data;
-    Node* next;
+  int data;
+  Node *next;
 };
 
 struct HashSet {
-    Node** set;
-    int size;
-    int num_elements;
-    double max_load_factor = 0.75;
+  Node **set;
+  int size;
+  int num_elements;
+  double max_load_factor;
 };
 
-HashSet* hashset_create(int initial_size, double max_load_factor);
+HashSet *hashset_create(int initial_size, double max_load_factor);
 
-void hashset_destroy(HashSet* set);
+void hashset_destroy(HashSet *set);
 
-bool hashset_add(HashSet* set, int value);
+bool hashset_add(HashSet *set, int value);
 
-bool hashset_remove(HashSet* set, int value);
+bool hashset_remove(HashSet *set, int value);
 
-bool hashset_contains(HashSet* set, int value);
+bool hashset_contains(HashSet *set, int value);
 
 uint32_t knuth_hash(int value);
 
-void hashset_resize(HashSet* set);
-
-
+void hashset_resize(HashSet *set);
 
 #endif
